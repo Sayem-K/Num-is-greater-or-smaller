f53ba979610bc95fed5154ae2c262fb21f220537
hoigoysohio <njuli@yahoo.com>
2024-12-11 18:41:29 +0000

    completed task 3

---
+++ main.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number: "))
+if num > 15:
+    print(num, "is greater than 15.")
+    print("I am part of if statement.")
+else:
+    print (num, "is less than 15")
+    print("I am part of else statement.")
+print("I am not part of else or if statement.")
